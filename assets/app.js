38b8df15f5a7ba52315ad203cebad9dbaac01562
Augustodsj1 <guto50@live.com>
2026-08-25 11:26:35 -0300

    Ajusta logos autorizadas e imagens de servicos

--- assets/app.js
+++ assets/app.js
@@ -98,7 +98,7 @@ function renderFooter() {
         <div class="footer-brand-panel">
           <div class="footer-certification" aria-label="Distribuidor autorizado Generac">
             <span class="footer-certification-label">Distribuidor autorizado</span>
-            <span class="footer-certification-brand">Generac</span>
+            <img src="assets/images/generac-assistencia-autorizada-white.png" alt="Generac Assistência Autorizada" class="footer-certification-logo">
           </div>
           <div class="footer-brand">
             ${logoImage}
